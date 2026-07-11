a8b6fa33e8827600d0e492b551634742c81531ed
Shivam Bhasin <shivambhasin@forwardnetworks.com>
2026-07-11 12:15:31 +0530

    Initial Commit

---
+++ echo/echoApp.swift
@@ -0,0 +1,17 @@
+//
+//  echoApp.swift
+//  echo
+//
+//  Created by Shivam Bhasin on 11/07/26.
+//
+
+import SwiftUI
+
+@main
+struct echoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
